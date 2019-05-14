34d5a74f36ec1241bab79082405e189f6b2ec7fe
Chad Carlson <chad.m.carlson@gmail.com>
2019-05-13 20:48:35 -0600

    static pages not working...

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :notes
+  root 'static_pages#home'
+  get '/about', to: 'static_pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
